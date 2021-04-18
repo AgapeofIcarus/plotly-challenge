effbc52dfbdac0451cd20249d0eb979a65e8e894
Chase Webb <webb.chase.a@gmail.com>
2021-04-18 10:32:19 -0400

    Working the daily code

--- static/js/app.js
+++ static/js/app.js
@@ -1,36 +1,14 @@
-const info = "../../Data/samples.json";
+const info = "../samples.json";
 
-// Fetch the JSON data and console log it
-d3.json(info).then(function(data) {
-  console.log(data);
-});
+//Get the data and generate the plots
+function getPlot(id) {
 
-const bellybutton = d3.json(info);
-console.log("Bellybutton Biodiversity: ", bellybutton);
-
-function unpack(rows, index) {
-    return rows.map(function(row) {
-      return row[index];
-    });
-  }
-
-function buildPlot() {
+    // Fetch the JSON data and console log it
     d3.json(info).then(function(data) {
-  
-      // Grab values from the data json object to build the plots
-      var names = data.dataset.names;
-      var metadata = data.dataset.metadata;
-      var samples = data.dataset.samples;
-
-      var trace1 = {
-        x: names,
-        y: eyeFlicker,
-        type: "bar"
-      };
-  
-      Plotly.newPlot("plot", data, layout);
-  
+        console.log(data);
     });
-  }
-  
-  buildPlot();
+
+    var washing = data.metadata.map(data => data.washing)
+    console.log(`Washing Frequency: ${washing}`)
+
+    
