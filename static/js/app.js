3d1ee9bdcd256037d8ab32c193993d138ef04ce3
Chase Webb <webb.chase.a@gmail.com>
2021-04-18 10:46:07 -0400

    Update app.js
    Just a lot of copy and paste, but we're making progress!

--- static/js/app.js
+++ static/js/app.js
@@ -13,4 +13,47 @@ function getPlot(id) {
 
     var samples = data.samples.filter(s => s.id.toString() === id)[0];
 
-        console.log(samples);
+    console.log(samples);
+
+    var values = samples.sample_values.slice(0, 10).reverse();
+
+    var otu_ids = (samples.otu_ids.slice(0, 10)).reverse();
+
+    var idOtu = idValues.map(data => "OTU " + data)
+
+    console.log(`OTU IDs: ${idOtu}`)
+
+    var labels = samples.otu_labels.slice(0, 10);
+
+    console.log(`Sample Values: ${samples}`)
+    
+    console.log(`Id Values: ${values}`)
+
+    //Lets get started on the actual plotting. We've got to trace.
+
+    var trace = {
+        x: values,
+        y: idOtu,
+        text: labels,
+        type:"bar",
+        orientation: "h",
+    };
+
+    var data = [trace];
+
+    var layout = {
+        title: "Top 10 OTUs",
+        yaxis:{
+            tickmode:"linear",
+        },
+        margin: {
+            l: 100,
+            r: 100,
+            t: 30,
+            b: 20
+        }
+    };
+
+    Plotly.newPlot("bar", data, layout);
+
+    
