ea83d0974f0a0e19eaca9c984642e5663b2687c5
Chase Webb <webb.chase.a@gmail.com>
2021-04-23 09:56:03 -0400

    Removed images
    Removed unnecessary images and adjusted html/js files.

--- Data_Html/app.js
+++ Data_Html/app.js
@@ -36,14 +36,100 @@ function buildcharts(selection) {
         var items = filteredData.filter(item => item.id == selection)[0];
         console.log(items);
 
+        // Lets get stuff for the barchart
         var values = items.sample_values; 
         var barChartValues = values.slice(0, 10).reverse();
         console.log(barChartValues);
 
-        var idValues = sampleDict.otu_ids;
+        var idValues = items.otu_ids;
         var barChartLabels = idValues.slice(0, 10).reverse();
         console.log("otu_ids");
         console.log(barChartLabels);
+
+        var reformattedLabels = [];
+        barChartLabels.forEach((label) => {
+            reformattedLabels.push("OTU " + label);
+        });
+
+        console.log("reformatted");
+        console.log(reformattedLabels);
+
+        var hovertext = items.otu_labels;
+        var barCharthovertext = hovertext.slice(0, 10).reverse();
+        console.log("otu_labels");
+        console.log(barCharthovertext);
+
+        //Now we're actually making the barchart.
+        var barChartTrace = {
+            type: "bar",
+            y: reformattedLabels,
+            x: barChartValues,
+            text: barCharthovertext,
+            orientation: 'h'
+        };
+
+        var barChartData = [barChartTrace];
+
+        Plotly.newPlot("bar", barChartData);
+
+        //Bubble chart time
+        var bubbleChartTrace = {
+            x: idValues,
+            y: sampleValues,
+            text: hovertext,
+            mode: "markers",
+            marker: {
+                color: idValues,
+                size: sampleValues
+            }
+        };
+
+        var bubbleChartData = [bubbleChartTrace];
+
+        var layout = {
+            showlegend: false,
+            height: 600,
+            width: 1000,
+            xaxis: {
+                title: "OTU ID"
+            }
+        };
+
+        Plotly.newPlot("bubble", bubbleChartData, layout);
+    });
 }
 
+//Lets make this run with the page load
+function init() {
+
+    // Read json data
+    d3.json("samples.json").then((Data) => {
+
+        // Parse and filter data to get sample names
+        var filteredData = Data.names;
+        console.log(filteredData);
+
+        // Add dropdown option for each sample
+        var dropdownMenu = d3.select("#selDataset");
+
+        parsedData.forEach((name) => {
+            dropdownMenu.append("option").property("value", name).text(name);
+        })
+
+        // Use first sample to build metadata and initial plots
+        buildMetadata(filteredData[0]);
+
+        buildCharts(filteredData[0]);
+
+    });
+}
+
+function optionChanged(newSelection) {
+
+    // Update metadata and charts
+    buildMetadata(newSelection); 
+    buildCharts(newSelection);
+}
+
+// Initialize dashboard on page load
 init();
